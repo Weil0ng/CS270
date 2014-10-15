5f7dea97b542d8cf400b9d23802057eaf253cb0f
Weil0ng <cuiweilong0205@gmail.com>
2014-10-14 18:24:12 -0700

    Use mmap instead of malloc

--- DiskEmulator.c
+++ DiskEmulator.c
@@ -7,21 +7,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+
+FILE *mapFile;
 
 //Disk size in bytes
 const UINT diskSize = 512;
 
 void initDisk(DiskArray *disk)
 {
-  disk->_dsk_dskArray = malloc(diskSize);
-  memset(disk->_dsk_dskArray, 0, diskSize);
+  mapFile = fopen("diskFile", "w+r");
+  if (mapFile == NULL)
+  {
+        printf("Disk map file open failed!");
+	exit(1);
+  }
+  ftruncate(fileno(mapFile), diskSize);
+  disk->_dsk_dskArray = mmap(NULL, diskSize, PROT_READ|PROT_WRITE, MAP_SHARED, fileno(mapFile), 0);
+  fclose(mapFile);
+  if (disk->_dsk_dskArray == MAP_FAILED)
+  {
+	printf("mmap failed!");
+	exit(1);
+  }
+  memset(disk->_dsk_dskArray, 0x1f, diskSize);
 
   disk->_dsk_numBlk = diskSize / BLK_SIZE;
 }
 
 void destroyDisk(DiskArray *disk)
 {
-  free(disk->_dsk_dskArray);
+  msync(disk->_dsk_dskArray, diskSize, MS_SYNC);
+  munmap(disk->_dsk_dskArray, diskSize);
 }
 
 int main(int args, char* argv[])
