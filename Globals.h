17fa91d433c03827249f31c31800e1a8a3629a34
JonathanSunUCSB <jsun@umail.ucsb.edu>
2014-12-06 10:54:37 -0800

    Extended file/path name length

--- Globals.h
+++ Globals.h
@@ -42,9 +42,9 @@
 
 #define OPEN_FILE_TABLE_LENGTH (1024)   //length of open file table
 #define INODE_TABLE_LENGTH (1024)   //number of bins in the hash queue of in core INodeTable
-#define FILE_NAME_LENGTH (32)      //number of bytes in the file name in bytes
+#define FILE_NAME_LENGTH (256)      //number of bytes in the file name in bytes
 
-#define MAX_PATH_LEN (100) //maximum length of the path
+#define MAX_PATH_LEN (512) //maximum length of the path
 #define MAX_FILE_SIZE (BLK_SIZE * INODE_NUM_DIRECT_BLKS + BLK_SIZE * INODE_NUM_S_INDIRECT_BLKS * (BLK_SIZE / sizeof(UINT)) + BLK_SIZE * INODE_NUM_D_INDIRECT_BLKS * (BLK_SIZE / sizeof(UINT)) * (BLK_SIZE / sizeof(UINT)) + BLK_SIZE * INODE_NUM_T_INDIRECT_BLKS * (BLK_SIZE / sizeof(UINT)) * (BLK_SIZE / sizeof(UINT)) * (BLK_SIZE / sizeof(UINT))) 
 #define MAX_FILE_BLKS (MAX_FILE_SIZE / BLK_SIZE) //max number of data blocks allocatable per file
 #define MAX_FILE_NUM_IN_DIR (MAX_FILE_SIZE / (FILE_NAME_LENGTH + sizeof(INT))) //maximum number of files in a directory
